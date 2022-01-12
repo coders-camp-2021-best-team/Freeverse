5e3bedadb4782c7dbf7eee61cfc71e3434abdc69
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-12 21:47:04 +0100

    refactor: Update `App` component to use `HelloWorld` component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { HelloWorld } from './components/';
 
-function App() {
+export const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className='app'>
+            <HelloWorld name={'Bob'} />
         </div>
     );
-}
-
-export default App;
+};
