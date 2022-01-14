e4bb6eb5bc8a2b8fc762bffc637647f9d6339b41
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-14 21:56:23 +0100

    feat: Add simple `Button` atom component

---
+++ src/components/atoms/Button/Button.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+import './Button.scss';
+
+export const Button = ({ text, variant, onClick }) => {
+    return (
+        <button
+            className={`atom__button atom__button__${variant}`}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
+};
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    variant: PropTypes.oneOf(['submit', 'primary', 'secondary', 'cancel']),
+    onClick: PropTypes.func
+};
+
+Button.defaultProps = {
+    variant: 'primary',
+    onClick: () => undefined
+};
