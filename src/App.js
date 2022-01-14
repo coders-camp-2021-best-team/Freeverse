92bad681d2d647cfdef53ef294e86b23e4d6a9a3
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-14 21:59:40 +0100

    refactor: Draw `Home` page in `App` main component

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { HelloWorld } from './components/';
-
-export const App = () => {
-    return (
-        <div className='app'>
-            <HelloWorld name={'Bob'} />
-        </div>
-    );
-};
