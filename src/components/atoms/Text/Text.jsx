38181c9ccc622b70fb650d29efa78efb70b7b2ef
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-17 21:56:57 +0100

    fix: Fix `className` typo in `Text` component

--- src/components/atoms/Text/Text.jsx
+++ src/components/atoms/Text/Text.jsx
@@ -4,7 +4,7 @@ import './Text.scss';
 
 export const Text = ({ children, type, size }) => {
     return (
-        <span className={`atom__text atom__text${type} atom__text${size}`}>
+        <span className={`atom__text atom__text__${type} atom__text__${size}`}>
             {children}
         </span>
     );
