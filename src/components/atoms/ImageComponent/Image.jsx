65e1e6a8019e52465bdb61afb2b75fde27a2d0fe
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-01-17 19:58:12 +0100

    fixed problems

--- src/components/atoms/ImageComponent/Image.jsx
+++ src/components/atoms/ImageComponent/Image.jsx
@@ -1,22 +1,21 @@
 import PropTypes from 'prop-types';
-
 import './Image.scss';
 
+// TODO Add loading on image onLoad
 
-export const ImageComponent = ({ src, size, onClick }) => {
+
+const imageOnClick = useCallback(() => onClick(), [onClick]);
+
+export const ImageComponent = memo(({ src, size, onClick }) => {
     return (
 
-        <img src={`${src}`} clasName={`atom__image atom_image_${size}`} onClick={() => imageClick()}></img>
+        <img src={`${src}`} clasName={`atom__image atom_image_${size}`} onClick={imageOnClick}></img>
 
     );
-};
+});
 
 ImageComponent.propTypes = {
     size: PropTypes.oneOf(['medium', 'large']),
+    source: img.propTypes.source.isRequired,
     onClick: PropTypes.func
-};
-
-ImageComponent.defaultProps = {
-    size: 'medium',
-    onClick: () => undefined
 };
