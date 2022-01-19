fe9de26feb55af0af4bc5d525a8f8681d27ce11a
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-19 20:05:09 +0100

    feat: Wrap `Router` in `App` with `AuthContextProvider`

--- src/App.jsx
+++ src/App.jsx
@@ -6,71 +6,8 @@ import 'typeface-roboto';
 
 export const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route exact path='/' element={<HomePage />} />
-
-                <Route exact path='*' element={<Navigate to='/not-found' />} />
-
-                <Route exact path='/not-found' element={<ErrorPage />} />
-
-                <Route
-                    exact
-                    path='/site/*'
-                    element={
-                        <BaseScreen>
-                            <Routes>
-                                <Route
-                                    exact
-                                    index
-                                    element={
-                                        <ProtectedRoute>
-                                            <FeedScreenPage />
-                                        </ProtectedRoute>
-                                    }
-                                />
-
-                                <Route
-                                    exact
-                                    path='/profile/:id'
-                                    element={
-                                        <ProtectedRoute>
-                                            <ProfilePage id={profileID} />
-                                        </ProtectedRoute>
-                                    }
-                                />
-
-                                <Route
-                                    exact
-                                    path='/chat'
-                                    element={
-                                        <ProtectedRoute>
-                                            <ChatPage />
-                                        </ProtectedRoute>
-                                    }
-                                />
-
-                                <Route
-                                    exact
-                                    path='/edit-profile'
-                                    element={
-                                        <ProtectedRoute>
-                                            <EditProfilePage />
-                                        </ProtectedRoute>
-                                    }
-                                />
-
-                                <Route
-                                    path='*'
-                                    element={
-                                        <Navigate to='/not-found' replace />
-                                    }
-                                />
-                            </Routes>
-                        </BaseScreen>
-                    }
-                />
-            </Routes>
-        </Router>
+        <AuthContextProvider>
+            <Router />
+        </AuthContextProvider>
     );
 };
