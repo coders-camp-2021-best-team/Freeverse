1adfb8b58f0a561dc437944db007c46e38218736
Konrad <trakon1e@gmail.com>
2022-01-22 22:27:17 +0100

    refactor: Import icons to IconComponent

--- src/components/atoms/IconComponent/Icon.jsx
+++ src/components/atoms/IconComponent/Icon.jsx
@@ -2,11 +2,35 @@ import './Icon.scss';
 import PropTypes from 'prop-types';
 import { useCallback } from 'react';
 import { ReactComponent as Like } from './icons/like.svg';
+import { ReactComponent as Avatar } from './icons/avatar.svg';
+import { ReactComponent as Birthday } from './icons/birthday.svg';
+import { ReactComponent as CircleX } from './icons/circleX.svg';
+import { ReactComponent as Comment } from './icons/comment.svg';
+import { ReactComponent as Dislike } from './icons/dislike.svg';
+import { ReactComponent as FortuneCookie } from './icons/fortuneCookie.svg';
+import { ReactComponent as GoogleController } from './icons/googleController.svg';
+import { ReactComponent as Home } from './icons/home.svg';
+import { ReactComponent as Logo } from './icons/logo.svg';
+import { ReactComponent as Power } from './icons/power.svg';
+import { ReactComponent as Send } from './icons/send.svg';
+import { ReactComponent as User } from './icons/user.svg';
 
 
 
 const icons = {
-    'like': Like
+    'like': Like,
+    'avatar': Avatar,
+    'birthday': Birthday,
+    'circleX': CircleX,
+    'comment': Comment,
+    'dislike': Dislike,
+    'fortuneCookie': FortuneCookie,
+    'googleController': GoogleController,
+    'home': Home,
+    'logo': Logo,
+    'power': Power,
+    'send': Send,
+    'user': User 
     }
 
 export const Icon = ({ iconName, size, className, onClick}) => {
@@ -26,7 +50,7 @@ export const Icon = ({ iconName, size, className, onClick}) => {
 
 /* eslint-disable react/no-typos */
 Icon.PropTypes = {
-    iconName: PropTypes.string.isRequired,
+    iconName: PropTypes.oneOf(Object.keys(icons)),
     size: PropTypes.oneOf(['small','medium', 'large']),
     onClick: PropTypes.func
 }
