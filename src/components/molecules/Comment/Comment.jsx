99fae2eaa732ab25043576cefbcc5ab6521c3f8d
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-01-23 14:02:47 +0100

    not done yet, little changed

--- src/components/molecules/Comment/Comment.jsx
+++ src/components/molecules/Comment/Comment.jsx
@@ -1,22 +1,27 @@
 import PropTypes from 'prop-types';
+import { Dayjs } from 'dayjs';
 import { Text } from '../../atoms/Text/Text';
 import '../../atoms/Text/Text.scss';
 import './Comment.scss';
 
-export const Comment = ({ username, date, commentText }) => {
+export const Comment = ({ username, date, children }) => {
     return (
         <div className='comment'>
             <div className='comment__info'>
-                <Text type='primary' size='small'>
-                    {date}
+                <Text type='primary' size='small' customClass='comment__info__date'>
+                    <Dayjs format='DD MM YYYY hh:mm'>{date}</Dayjs>
                 </Text>
-                <Text type='accent' size='medium'>
+                <Text type='accent' size='medium' customClass='comment__info__username'>
                     {username}
                 </Text>
             </div>
             <div className='comment__text'>
-                <Text type='primary' size='medium'>
-                    {commentText}
+                <Text
+                    type='primary'
+                    size='medium'
+                    customClass='comment__text__p'
+                >
+                    {children}
                 </Text>
             </div>
         </div>
@@ -24,7 +29,7 @@ export const Comment = ({ username, date, commentText }) => {
 };
 
 Comment.propTypes = {
-    commentText: PropTypes.string.isRequired,
+    children: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired
 };
