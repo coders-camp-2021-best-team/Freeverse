e02bd8a92b064fb3fd51f6f572fe2206a9b2a0b9
Mateusz Buczek <85763715+T0ffik@users.noreply.github.com>
2022-01-23 14:13:53 +0100

    feat: add useResponsive custom hook (#51)
    * feat: add useResponsive custom hook
    
    * refactor: include comments

---
+++ src/utils/useResponsive.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+export const useResponsive = () => {
+    const [res, setRes] = useState(window.innerWidth);
+
+    const changeRes = () => setRes(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', changeRes());
+        return () => {
+            window.removeEventListener('resize', () => changeRes());
+        };
+    }, []);
+    return {
+        isDesktop: res >= 1501,
+        isTablet: res >= 600 && res < 1501,
+        isPhone: res < 600
+    };
+};
