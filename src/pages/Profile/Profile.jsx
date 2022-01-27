d765b2f510b754b4cfb22457a7168b2d669b9fcf
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-27 23:02:12 +0100

    feat: Add example usage to `ProfilePage`

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
 import { useAuth } from '../../hooks';
+import { apiProvider } from '../../api';
 
 export const ProfilePage = () => {
     const { user } = useAuth();
+
+    const [chatRooms, setChatRooms] = useState();
+    const [posts, setPosts] = useState();
+
+    useEffect(() => {
+        const fetch = async () => {
+            setChatRooms(await apiProvider.getUserChatRooms(user.uid));
+            setPosts(await apiProvider.getUserPosts(user.uid));
+        };
+        fetch();
+    });
 
     return (
         <>
@@ -9,6 +22,12 @@ export const ProfilePage = () => {
             <div>{`Hello, ${user.displayName}!`}</div>
             <pre>
                 <code>{JSON.stringify(user, null, 4)}</code>
+            </pre>
+            <pre>
+                <code>{JSON.stringify(chatRooms, null, 4)}</code>
+            </pre>
+            <pre>
+                <code>{JSON.stringify(posts, null, 4)}</code>
             </pre>
         </>
     );
