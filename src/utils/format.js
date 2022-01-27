414cc8f5425afeb38540a6209505159b80f62051
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-01-27 20:46:48 +0100

    use arrow function

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,5 +1,5 @@
 import * as dayjs from 'dayjs';
 
-export function dateFormat(date, format = 'DD MM YYYY hh:mm') {
+export const dateFormat = (date, format = 'DD MM YYYY hh:mm') => {
     return dayjs(date).format(format);
-}
+};
