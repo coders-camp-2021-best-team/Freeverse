bc04bb186e91ce3edb8a71c224c312be4a1683e6
T0ffik <t0fffik2000@gmail.com>
2022-01-28 12:52:48 +0100

    feat: Add form component

---
+++ src/components/organisms/AddForm/AddForm.jsx
@@ -0,0 +1,66 @@
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import PropTypes from 'prop-types';
+import { Icon } from '../..';
+
+import './Form.scss';
+
+export const AddForm = ({ placeholder, type }) => {
+    const submitOnEnter = (event) => {
+        if (event.key === 'Enter' && event.shiftKey === false) {
+            event.preventDefault();
+            event.target.form.dispatchEvent(
+                new Event('submit', { cancelable: true, bubbles: true })
+            );
+        }
+    };
+
+    const onSubmit = (values, action) => {
+        // TODO temporary, connect with API
+        console.log(values);
+        action.resetForm({
+            values: {
+                [`${type}`]: ''
+            }
+        });
+    };
+
+    const focusOnInput = (event) => {
+        event.target[0].focus();
+    };
+
+    const validation = Yup.object().shape({
+        [`${type}`]: Yup.string().required('Required')
+    });
+
+    return (
+        <div className={`form form__${type}`} role='directory' onClick={focusOnInput}>
+            <Formik
+                initialValues={{ name: type }}
+                validationSchema={validation}
+                onSubmit={onSubmit}
+            >
+                {({ handleSubmit, errors, touched }) => (
+                    <Form>
+                        <Field
+                            name={type}
+                            as='textarea'
+                            placeholder={placeholder}
+                            validate={validation}
+                            onKeyDown={submitOnEnter}
+                        />
+                        {errors.type && touched.type ? (
+                            <div>{errors.type}</div>
+                        ) : null}
+                        <Icon iconName='send' onClick={handleSubmit} />
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+};
+
+AddForm.propTypes = {
+    placeholder: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['comment', 'post', 'chat']).isRequired
+};
--- src/pages/BaseScreen/BaseScreen.jsx
+++ src/pages/BaseScreen/BaseScreen.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import { Header } from '../../components';
+import { AddForm } from '../../components/organisms/AddForm/AddForm';
 
 export const BaseScreen = ({ children }) => {
     return (
         <>
             <Header />
             <main>{children}</main>
+            <AddForm placeholder='Write something...' type='post' />
         </>
     );
 };
