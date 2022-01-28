4a5836b075e994081e3ad6852530ff9df090b73a
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-28 22:47:55 +0100

    feat: Add custom error handling using `react-toastify`

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { AuthContextProvider } from './utils';
+import { ToastContainer, toast } from 'react-toastify';
+import { AuthContextProvider, UserContextProvider } from './utils';
 import { Router } from './Router';
 
+import 'react-toastify/dist/ReactToastify.css';
 import './styles/global.scss';
 import 'typeface-roboto';
 
 export const App = () => {
+    window.onerror = (event) => {
+        toast(event.toString(), {
+            type: 'error'
+        });
+    };
+
+    window.onunhandledrejection = (e) => {
+        throw new Error(e.reason.message);
+    };
+
     return (
-        <AuthContextProvider>
-            <Router />
-        </AuthContextProvider>
+        <>
+            <ToastContainer theme='dark' autoClose={5000} />
+            <AuthContextProvider>
+                <UserContextProvider>
+                    <Router />
+                </UserContextProvider>
+            </AuthContextProvider>
+        </>
     );
 };
