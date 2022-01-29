039c5bd40d2b36e13e456501a0eee45efe925047
T0ffik <t0fffik2000@gmail.com>
2022-01-29 23:19:30 +0100

    refactor: delete form from BaseScreen

--- src/pages/BaseScreen/BaseScreen.jsx
+++ src/pages/BaseScreen/BaseScreen.jsx
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 import { Header } from '../../components';
-import { AddForm } from '../../components/organisms/AddForm/AddForm';
 
 export const BaseScreen = ({ children }) => {
     return (
         <>
             <Header />
             <main>{children}</main>
-            <AddForm placeholder='Add comment' type='chat' />
         </>
     );
 };
