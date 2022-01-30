fa8ad3dbdf9803f779949c2dc21325735c564b97
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-01-30 12:01:14 +0100

    changes

--- src/components/molecules/InformationRow/InformationRow.jsx
+++ src/components/molecules/InformationRow/InformationRow.jsx
@@ -3,14 +3,14 @@ import { Text } from '../../atoms/Text/Text';
 import { Icon } from '../../atoms/IconComponent/Icon';
 import './InformationRow.scss';
 
-export const InformationRow = ({ onClick, children, src }) => {
+export const InformationRow = ({ onClick, children, nameIcon }) => {
     return (
         <div
             onClick={onClick}
             role='complementary'
             className='information__row'
         >
-            <Icon size='medium' iconName={src} />
+            <Icon size='medium' iconName={nameIcon} />
             <Text type='primary' size='medium'>
                 {children}
             </Text>
@@ -20,7 +20,7 @@ export const InformationRow = ({ onClick, children, src }) => {
 
 InformationRow.propTypes = {
     children: PropTypes.string.isRequired,
-    src: PropTypes.string.isRequired,
+    nameIcon: PropTypes.symbol.isRequired,
     onClick: PropTypes.func
 };
 
