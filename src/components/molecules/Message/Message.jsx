2a692161491712155fb8a1142d759e7ccafce930
T0ffik <t0fffik2000@gmail.com>
2022-02-01 22:52:15 +0100

    feat: create message component

---
+++ src/components/molecules/Message/Message.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { ImageComponent, Text } from '../..';
+
+import './Message.scss';
+
+export const MessageComponent = ({ children, date, name, avatar }) => {
+    return (
+        <div className='message__field'>
+            <ImageComponent src={avatar} size='small' onClick={() => console.log('dupa')} />
+            <Text size='small' customClass='name'>
+                {name}
+            </Text>
+            <Text size='small' customClass='date'>
+                {date}
+            </Text>
+            <Text type='primary' customClass='message'>
+                {children}
+            </Text>
+        </div>);
+};
+
+MessageComponent.propTypes = {
+    children: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+};
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,3 +1,12 @@
+import { MessageComponent } from '../../components';
+import avatar from '../../images/avatar.png';
+
 export const ChatPage = () => {
-    return <div>This is chat page</div>;
+    const user = {
+        name: 'Batman',
+    };
+    return (
+        <MessageComponent date='21.12.2021  10:40 AM' name={user.name} avatar={avatar}>
+            elo elo elo wiadoność 1,2,3
+        </MessageComponent>);
 };
