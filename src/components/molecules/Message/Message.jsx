3c15aab00a9953617eb775a60410700f57bfb17a
T0ffik <t0fffik2000@gmail.com>
2022-02-02 00:44:56 +0100

    fix: fix errors agian

--- src/components/molecules/Message/Message.jsx
+++ src/components/molecules/Message/Message.jsx
@@ -1,10 +1,21 @@
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import { ImageComponent, Text } from '../..';
+import { routes } from '../../../routes/Routes';
 import chatAvatar from '../../../images/avatar.png';
 
 import './Message.scss';
 
-export const MessageComponent = ({ children, date, name, avatar, isYours }) => {
+export const MessageComponent = ({
+    children,
+    date,
+    name,
+    avatar,
+    isYours,
+    profileID
+}) => {
+    const redirect = useNavigate();
+
     const stringDate = () => {
         const mm = date.getMonth() + 1;
         const dd = date.getDate();
@@ -23,7 +34,11 @@ export const MessageComponent = ({ children, date, name, avatar, isYours }) => {
 
     return (
         <div className={`message__field ${isYours ? 'ownMessage' : ''}`}>
-            <ImageComponent src={avatar} size='small' onClick />
+            <ImageComponent
+                src={avatar}
+                size='small'
+                onClick={() => { redirect(`${routes.Feed}${routes.Profile}/${profileID}`); }}
+            />
             <Text size='small' customClass='name'>
                 {name}
             </Text>
@@ -42,7 +57,8 @@ MessageComponent.propTypes = {
     date: PropTypes.instanceOf(Date).isRequired,
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string,
-    isYours: PropTypes.bool
+    isYours: PropTypes.bool,
+    profileID: PropTypes.string.isRequired
 };
 
 MessageComponent.defaultProps = {
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,16 +1,19 @@
 import { MessageComponent } from '../../components';
 
 export const ChatPage = () => {
+    // TODO temporary values
     const currentDate = new Date();
     const user = {
         name: 'Batman',
-        you: false
+        you: false,
+        id: 'jP-2I-E7'
     };
     return (
         <MessageComponent
             date={currentDate}
             name={user.name}
             isYours={user.you}
+            profileID={user.id}
         >
             Lorem ipsum
         </MessageComponent>
