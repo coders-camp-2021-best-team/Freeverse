3ef36a8cad4ad9b62896f223778b0a1ad93ac6ea
T0ffik <t0fffik2000@gmail.com>
2022-02-02 17:35:23 +0100

    refactor: format files

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -6,7 +6,9 @@ import './Chat.scss';
 export const ChatPage = () => {
     return (
         <div className='page__chat'>
-            <Text size='extraLarge' customClass='title'>Public chat room</Text>
+            <Text size='extraLarge' customClass='title'>
+                Public chat room
+            </Text>
             <div className='chat'>
                 <MessageCollection usersMsgs={usersMsgs} />
                 <Form placeholder='Write message...' type='chat' onSubmit />
