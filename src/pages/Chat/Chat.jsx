97cf91416ffae8513da3ed5b03b54312d461e6a5
T0ffik <t0fffik2000@gmail.com>
2022-02-02 17:34:54 +0100

    feat: create chat screen

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,6 +1,16 @@
-import { MessageCollection } from '../../components';
+import { MessageCollection, Form, Text } from '../../components';
 import { usersMsgs } from '../../utils/Mocks';
 
+import './Chat.scss';
+
 export const ChatPage = () => {
-    return <MessageCollection usersMsgs={usersMsgs} />;
+    return (
+        <div className='page__chat'>
+            <Text size='extraLarge' customClass='title'>Public chat room</Text>
+            <div className='chat'>
+                <MessageCollection usersMsgs={usersMsgs} />
+                <Form placeholder='Write message...' type='chat' onSubmit />
+            </div>
+        </div>
+    );
 };
