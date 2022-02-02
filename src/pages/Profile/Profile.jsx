e02ba928f0536cc832fa08170583f64ca7cec969
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-02 23:40:59 +0100

    fix: Fix `ProfilePage`

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,22 +1,54 @@
-import { useAuth, useUser } from '../../hooks';
-import { apiService } from '../../api';
+import PropTypes from 'prop-types';
+import { useParams } from 'react-router';
+import { Timestamp } from 'firebase/firestore';
+import {
+    useAuth,
+    useUserDetails,
+    useUserPosts,
+    useUserChatRooms,
+    usePublicChatRooms
+} from '../../hooks';
 import { Button } from '../../components';
 
-export const ProfilePage = () => {
-    const { user } = useAuth();
-    const { userDetails, userPosts, userChatRooms } = useUser();
+export const ProfilePage = ({ id }) => {
+    const params = useParams();
+    const userID = id || params.id;
+
+    const {
+        user: { data: user }
+    } = useAuth();
+    const { data: userDetails } = useUserDetails(userID);
+    const { data: userPosts, create: createPost } = useUserPosts(userID);
+    const { data: userChatRooms, create: createChatRoom } =
+        useUserChatRooms(userID);
+    const { data: publicChatRooms } = usePublicChatRooms();
 
     return (
         <>
             <div>This is profile page</div>
             <div>{`Hello, ${user.displayName}!`}</div>
-            {/* <pre>
+            <pre>
                 <code>{JSON.stringify(user, null, 4)}</code>
-            </pre> */}
+            </pre>
             <pre>
                 <code>
                     {JSON.stringify(
-                        { userDetails, userPosts, userChatRooms },
+                        {
+                            userID,
+                            userDetails: userDetails?.data(),
+                            userPosts: userPosts?.docs.map((d) => ({
+                                id: d.id,
+                                ...d.data()
+                            })),
+                            userChatRooms: userChatRooms?.docs.map((d) => ({
+                                id: d.id,
+                                ...d.data()
+                            })),
+                            publicChatRooms: publicChatRooms?.docs.map((d) => ({
+                                id: d.id,
+                                ...d.data()
+                            }))
+                        },
                         null,
                         4
                     )}
@@ -24,34 +56,60 @@ export const ProfilePage = () => {
             </pre>
             <div role='form'>
                 <Button
-                    text='create new chat room'
+                    text='create new private chat room'
                     onClick={() => {
                         // eslint-disable-next-line no-alert
                         const name = prompt(
                             'Chat room name:',
-                            `new-chat-room-${Date.now()}`
+                            `private-chat-room-${Date.now()}`
                         );
 
-                        apiService.createChatRoom(name, {
+                        createChatRoom({
+                            name,
                             admins: [user.uid],
                             members: [user.uid]
                         });
                     }}
                 />
-
                 <Button
-                    text='delete chat room'
+                    text='create new public chat room'
                     onClick={() => {
                         // eslint-disable-next-line no-alert
                         const name = prompt(
                             'Chat room name:',
-                            `new-chat-room-${Date.now()}`
+                            `public-chat-room-${Date.now()}`
                         );
 
-                        apiService.deleteChatRoom(name);
+                        createChatRoom({
+                            name,
+                            admins: [user.uid],
+                            members: null
+                        });
+                    }}
+                />
+                <Button
+                    text='create new post'
+                    onClick={() => {
+                        // eslint-disable-next-line no-alert
+                        const content = prompt('Post content:');
+
+                        createPost({
+                            authorID: user.uid,
+                            createdOn: Timestamp.now(),
+                            reactions: {},
+                            text_content: content
+                        });
                     }}
                 />
             </div>
         </>
     );
+};
+
+ProfilePage.propTypes = {
+    id: PropTypes.string
+};
+
+ProfilePage.defaultProps = {
+    id: null
 };
