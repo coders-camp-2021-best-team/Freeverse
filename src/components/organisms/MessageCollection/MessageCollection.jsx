bb7fce7414ccafb8187e3bd5620a664bda4d1459
Mateusz Buczek <85763715+T0ffik@users.noreply.github.com>
2022-02-02 16:06:18 +0100

    Feat: add MessageCollection (#64)
    * refactor: add MessageCollection
    
    * refactor: comments included
    
    * refactor: change proptype

---
+++ src/components/organisms/MessageCollection/MessageCollection.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import { MessageComponent } from '../..';
+
+export const MessageCollection = ({ usersMsgs }) => {
+    return usersMsgs.map((user) => (
+        <MessageComponent
+            date={user.date}
+            name={user.name}
+            avatar={user.avatar}
+            isYours={user.isYour}
+            profileID={user.profileId}
+            key={user.ID}
+        >
+            {user.message}
+        </MessageComponent>
+    ));
+};
+
+MessageCollection.propTypes = {
+    usersMsgs: PropTypes.arrayOf(Object).isRequired
+};
