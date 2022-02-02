666a85b1710d53cc545f984ee9c05e9b1a1864ab
T0ffik <t0fffik2000@gmail.com>
2022-02-02 13:06:03 +0100

    refactor: add margin bottom on message

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -8,14 +8,32 @@ export const ChatPage = () => {
         you: false,
         id: 'jP-2I-E7'
     };
+    const userSecond = {
+        name: 'Superman',
+        you: true,
+        id: 'G-M-D'
+    };
     return (
-        <MessageComponent
-            date={currentDate}
-            name={user.name}
-            isYours={user.you}
-            profileID={user.id}
-        >
-            Lorem ipsum
-        </MessageComponent>
+        <>
+            <MessageComponent
+                date={currentDate}
+                name={user.name}
+                isYours={user.you}
+                profileID={user.id}
+            >
+                Lorem ipsum
+            </MessageComponent>
+            <MessageComponent
+                date={currentDate}
+                name={userSecond.name}
+                isYours={userSecond.you}
+                profileID={userSecond.id}
+            >
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Architecto velit vero incidunt eligendi sit modi unde delectus
+                distinctio ex recusandae amet,
+                expedita nihil harum natus voluptas, necessitatibus voluptates, asperiores commodi.
+            </MessageComponent>
+        </>
     );
 };
