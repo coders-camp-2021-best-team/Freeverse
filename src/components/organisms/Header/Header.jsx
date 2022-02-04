e9d726be38b139c21a28519c6ff8441e5d642f4a
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-02-04 20:48:34 +0100

    fix

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,4 +1,6 @@
 import { useState, useCallback } from 'react';
+import { NavLink } from 'react-router-dom';
+import { routes } from '../../../routes/Routes';
 import { ReactComponent as Logo } from '../../../images/logo.svg';
 import { UserInfo } from '../../molecules/UserInfo/UserInfo';
 import { Navigation } from '../Navigation/Navigation';
@@ -16,7 +18,9 @@ export const Header = () => {
 
     return (
         <header className='nav__bar'>
-            <Logo className='nav__bar__logo' />
+            <NavLink to={`${routes.Feed}`}>
+                <Logo className='nav__bar__logo' />
+            </NavLink>
             <div className='nav__bar__section'>
                 <UserInfo onClick={handleOnImgClick} />
                 <Navigation isOpen={isNavigationOpen} />
