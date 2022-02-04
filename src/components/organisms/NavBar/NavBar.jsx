75e8e2434453b9e1529b85b694ccbb8c48e4e7e4
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-02-04 16:51:42 +0100

    Navbar

--- src/components/organisms/NavBar/NavBar.jsx
+++ src/components/organisms/NavBar/NavBar.jsx
@@ -24,7 +24,7 @@ export const NavBar = () => {
                     src={COOKIE}
                     alt=''
                     size='large'
-                    onClick={() => console.log('tekst')}
+                    onClick
                 />
             </div>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ export const HomePage = () => {
             <Logo className='home__page__logo' />
             <Button
                 text='LOG IN WITH GOOGLE'
-                onClick={() => console.log('log in')}
+                onClick
             />
         </div>
     );
