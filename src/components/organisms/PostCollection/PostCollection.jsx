7148384936b109eebbdcdfc83bdbda58f69701a4
Konrad <trakon1e@gmail.com>
2022-02-06 21:31:03 +0100

    Correct typo

--- src/components/organisms/PostCollection/PostCollection.jsx
+++ src/components/organisms/PostCollection/PostCollection.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { Post } from '../..';
 
 export const PostCollection = ({ userPosts }) => {
-    return userPosts.map(({ postID }) => <Post postID={postID} />);
+    return userPosts.map((postID) => <Post postID={postID} />);
 };
 
 PostCollection.propTypes = {
