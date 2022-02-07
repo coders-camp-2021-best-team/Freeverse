2a2fc5a79e7ef8352075c901f822e628ca95a47d
Norbert Jankowski <norbert.jankowski.akademia@gmail.com>
2022-02-07 23:38:32 +0100

    Added UserDetails organism, added default background image

---
+++ src/components/organisms/UserDetails/UserDetails.jsx
@@ -0,0 +1,14 @@
+import { InformationRow, Text } from '../..';
+
+import './UserDetails.scss';
+
+export const UserDetails = () => {
+    return (
+        <div>
+            <Text size='medium'>User Details</Text>
+            <InformationRow iconName='birthday'>xd</InformationRow>
+            <InformationRow iconName='home'>xd2</InformationRow>
+            <InformationRow iconName='googleController'>xd3</InformationRow>
+        </div>
+    );
+};
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router';
-import { ImageComponent, UserInfo } from '../../components';
+import { ImageComponent, UserInfo, UserDetails } from '../../components';
 import { useUserDetails } from '../../hooks';
+import DEFAULT_BACKGROUND from '../../images/background.png';
 
 import './Profile.scss';
 
@@ -8,16 +9,20 @@ export const ProfilePage = () => {
     const params = useParams();
     const userID = params.id;
     const { data: userData } = useUserDetails(userID);
+    const background = userData.background_image_url
+        ? userData.background_image_url
+        : DEFAULT_BACKGROUND;
 
     return (
         <div className='profile__page'>
             <div>
                 <ImageComponent
                     customClass='background__image'
-                    src={userData.background_picture_url}
+                    src={background}
                 />
             </div>
             <UserInfo userID={userID} customClass='profile__page__user__info' />
+            <UserDetails />
         </div>
     );
 };
