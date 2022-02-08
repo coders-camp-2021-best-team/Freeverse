a9eb36f7aa0e12743e42368b84994a37e203f321
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-08 10:11:57 +0100

    feat: Update `countReactions` util method

--- src/utils/Reactions.js
+++ src/utils/Reactions.js
@@ -1,15 +1,23 @@
 /**
- * @param {import('../api/types').Reaction[]} reactions
+ * @param {{ id: string, data: import('../api/types').Reaction }[]} reactions
  */
-export const countReactions = (reactions) => {
+export const countReactions = (reactions, userID) => {
     return {
         likes: reactions.reduce(
-            (count, { type }) => (type === 'LIKE' ? count + 1 : count),
+            (count, { data: { type } }) =>
+                type === 'LIKE' ? count + 1 : count,
             0
         ),
+        likedBy: reactions.some(
+            ({ id, data: { type } }) => id === userID && type === 'LIKE'
+        ),
         dislikes: Object.values(reactions).reduce(
-            (count, { type }) => (type === 'DISLIKE' ? count + 1 : count),
+            (count, { data: { type } }) =>
+                type === 'DISLIKE' ? count + 1 : count,
             0
+        ),
+        dislikedBy: reactions.some(
+            ({ id, data: { type } }) => id === userID && type === 'DISLIKE'
         )
     };
 };
